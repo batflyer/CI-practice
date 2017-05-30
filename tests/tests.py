b530dbefeff477d1a20d27b43de12a88f3c6d1dd
Alexander L. Hayes <alexander@batflyer.net>
2017-05-30 08:27:23 -0400

    adding requirements and initial tests.

---
+++ tests/tests.py
@@ -0,0 +1,10 @@
+# Tests for CI-practice
+import os
+import sys
+import numpy
+import pickle
+import pandas
+import sklearn.ensemble as ske
+from sklearn import cross_validation, tree, linear_model
+
+print('Success says I.')
