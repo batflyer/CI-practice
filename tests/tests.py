95d390aa5eb7ce910f5d42780c2a81d020f6e8b1
Alexander L. Hayes <alexander@batflyer.net>
2017-05-30 09:13:46 -0400

    more test cases to try out.

--- tests/tests.py
+++ tests/tests.py
@@ -4,7 +4,28 @@ import sys
 import numpy
 import pickle
 import pandas
+import unittest
 import sklearn.ensemble as ske
 from sklearn import cross_validation, tree, linear_model
 
-print('Success says I.')
+class BasicFunctions:
+
+    def function(self, x):
+        return x + 1
+
+    def function2(self, x):
+        return [1,2,3]
+
+class MyTest(unittest.TestCase):
+
+    def test_assert_true(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+    
+    def test_functions_above(self):
+        f = BasicFunctions()
+        self.assertEqual(f.function(3),4)
+        self.assertEqual(f.function2(5),[1,2,3])
+
+if __name__ == '__main__':
+    unittest.main()
